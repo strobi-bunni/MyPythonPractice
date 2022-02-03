1b17b8ff586b22fd33c04af2dc89a4c0a0a19f03
strobi-bunni <dbrlgus8520@gmail.com>
2022-02-03 17:15:42 +0900

    refine codebase and add user-friendly funcs

--- lib/compounds.py
+++ lib/compounds.py
@@ -1,3 +1,4 @@
+import re
 from typing import Optional, Sequence, Union
 
 T_JSON_Primitive = Union[str, bool, None, int, float]
@@ -8,14 +9,20 @@ T_JSON_Container = Union[T_JSON_List, T_JSON_Object]
 
 
 # 리스트에 항목을 추가한다는 뜻의 전용 싱글톤
-class _NewListItemSingleton:
+class NewListItemSingleton:
     def __new__(cls):
         if not hasattr(cls, 'instance'):
-            cls.instance = super(_NewListItemSingleton, cls).__new__(cls)
+            cls.instance = super(NewListItemSingleton, cls).__new__(cls)
         return cls.instance  # noqa
 
+    def __repr__(self):
+        return 'NewListItemSingleton()'
 
-NEW_LIST_ITEM = _NewListItemSingleton()  # keys에 이게 있으면 리스트에 새 항목을 만든다.
+
+NEW_LIST_ITEM = NewListItemSingleton()  # keys에 이게 있으면 리스트에 새 항목을 만든다.
+NEW_LIST_SYMBOL = '+'
+JSON_OBJECT_SEP = '.'
+re_interger = re.compile(r'^[+\-]?\d+$')
 
 
 def _is_primitive(item: T_JSON_Object) -> bool:
@@ -27,7 +34,7 @@ def _is_primitive(item: T_JSON_Object) -> bool:
 
 
 # TODO: 테스트할 것
-def get_item_tree(data: T_JSON_Container, *keys: Union[str, int], default=None, strict=False) -> T_JSON_Types:
+def get_json_tree_item(data: T_JSON_Container, *keys: Union[str, int], default=None, strict=False) -> T_JSON_Types:
     # TODO: 문서 추가
     # 종료 조건: keys가 없으면 그대로를 반환
     if not keys:
@@ -39,7 +46,7 @@ def get_item_tree(data: T_JSON_Container, *keys: Union[str, int], default=None, 
     # 딕셔너리(Python) / 객체(JSON)
     if isinstance(data, dict):
         if keys_head in data:
-            return get_item_tree(data[keys_head], *keys_tail, default=default, strict=strict)
+            return get_json_tree_item(data[keys_head], *keys_tail, default=default, strict=strict)
         elif strict:
             raise KeyError(f'Key not found: {keys_head}')
         else:
@@ -55,7 +62,7 @@ def get_item_tree(data: T_JSON_Container, *keys: Union[str, int], default=None, 
         if strict and not (-len(data) <= keys_head < len(data)):  # 음수 인덱스 고려
             raise IndexError(f'Index out of range: {keys_head}')
 
-        return get_item_tree(data[keys_head], *keys_tail, default=default, strict=strict)
+        return get_json_tree_item(data[keys_head], *keys_tail, default=default, strict=strict)
 
     # 기본 자료형
     elif strict:
@@ -65,7 +72,7 @@ def get_item_tree(data: T_JSON_Container, *keys: Union[str, int], default=None, 
 
 
 def _prepare_container_for_dict(data: dict, keys_head: Union[str, int],
-                                keys_tail: Sequence[Union[str, int]] = ()) -> None:
+                                keys_tail: Sequence[Union[str, int]]) -> None:
     if keys_tail[0] is NEW_LIST_ITEM:
         data[keys_head] = []
     else:
@@ -82,7 +89,7 @@ def _prepare_container_for_list(data: list, keys_head: Optional[Union[str, int]]
 
 
 # TODO: 테스트할 것
-def set_item_tree_value(data: T_JSON_Container, *keys: Union[str, int], value=None) -> None:
+def set_json_tree_item(data: T_JSON_Container, *keys: Union[str, int], value=None) -> None:
     # 종료 조건: key가 없으면 아무것도 하지 않는다.
     if not keys:
         return
@@ -96,19 +103,13 @@ def set_item_tree_value(data: T_JSON_Container, *keys: Union[str, int], value=No
         # 꼬리가 없다면 최종적으로 값을 넣는다.
         if not keys_tail:
             data[keys_head] = value
-        # 꼬리가 있으면서 data에 머리가 없다면: 새 컨테이너를 만든다.
+        # 새 컨테이너를 만드는 조건
+        # 1. 꼬리가 있지만 머리가 없다
+        # 2. 꼬리가 있지만 data의 머리에 해당되는 값이 원시자료형이다
         else:
-            if keys_head not in data:
+            if keys_head not in data or _is_primitive(data[keys_head]):
                 _prepare_container_for_dict(data, keys_head, keys_tail)
-            # 꼬리가 있으면서 data에 머리가 있다면: 머리에 따라서 동작이 달라짐
-            else:
-                if _is_primitive(data[keys_head]):
-                    # 만약에 원시타입이면 새 컨테이너를 만들고 재귀한다.
-                    _prepare_container_for_dict(data, keys_head, keys_tail)
-                    set_item_tree_value(data[keys_head], *keys_tail, value=value)
-                else:
-                    # 만약 컨테이너라면 재귀한다.
-                    set_item_tree_value(data[keys_head], *keys_tail, value=value)
+            set_json_tree_item(data[keys_head], *keys_tail, value=value)
 
     elif isinstance(data, list):
         # 꼬리 유무로 판단하는 것이 좋을 듯.
@@ -127,14 +128,13 @@ def set_item_tree_value(data: T_JSON_Container, *keys: Union[str, int], value=No
             if keys_head is NEW_LIST_ITEM:
                 # 꼬리가 있으면서 현재 리스트에 새 항목을 만든다 -> 리스트에 새 컨테이너를 추가한다.
                 _prepare_container_for_list(data, None, keys_tail)
+                set_json_tree_item(data[-1], *keys_tail, value=value)
             elif isinstance(keys_head, int) and (-len(data) <= keys_head < len(data)):
                 # 꼬리가 있으면서 머리가 유효한 리스트 인덱스다:
                 if _is_primitive(data[keys_head]):
                     # 만약에 원시타입이면 새 컨테이너를 만들고 재귀한다
                     _prepare_container_for_list(data, keys_head, keys_tail)
-                    set_item_tree_value(data[keys_head], *keys_tail, value=value)
-                else:
-                    set_item_tree_value(data[keys_head], *keys_tail, value=value)
+                set_json_tree_item(data[keys_head], *keys_tail, value=value)
             else:
                 # 유효한 인덱스가 아니면 에러 처리
                 raise IndexError(f'Invalid index {keys_head} for list {data}')
@@ -144,7 +144,7 @@ def set_item_tree_value(data: T_JSON_Container, *keys: Union[str, int], value=No
 
 
 # TODO: 테스트할 것
-def delete_item_tree(data: T_JSON_Container, *keys: Union[str, int]) -> None:
+def delete_json_tree_item(data: T_JSON_Container, *keys: Union[str, int], strict=False) -> None:
     # 종료 조건: key가 없으면 아무것도 하지 않는다.
     if not keys:
         return
@@ -156,27 +156,55 @@ def delete_item_tree(data: T_JSON_Container, *keys: Union[str, int]) -> None:
         # 만약에 꼬리가 없다면: 머리에 해당되는 값을 지운다.
         if not keys_tail:
             data.pop(keys_head, None)
-        else:
+        elif keys_head in data:
             # 만약에 꼬리가 있다면 꼬리에 해당되는 값을 지운다.
-            if keys_head in data:
-                delete_item_tree(data[keys_head], *keys_tail)
-            else:
-                pass  # TODO: 여기에 strict=True일 때 동작?
+            delete_json_tree_item(data[keys_head], *keys_tail)
+        elif strict:
+            raise KeyError(f'Key {keys_head} not found')
     elif isinstance(data, list):
         # 만약에 꼬리가 없다면: 머리에 해당되는 값을 지운다.
         if not keys_tail:
             if isinstance(keys_head, int) and (-len(data) <= keys_head < len(data)):
                 data.pop(keys_head)
-            else:
-                pass  # TODO: strict=True일 떄 동작?
+            elif strict:
+                raise KeyError(f'Invalid index')
+
         else:
             # 만약에 꼬리가 있다면 꼬리에 해당되는 값을 지운다.
             if isinstance(keys_head, int) and (-len(data) <= keys_head < len(data)):
-                delete_item_tree(data[keys_head], *keys_tail)
-            else:
-                pass  # TODO: 여기에 strict=True일 때 동작?
+                delete_json_tree_item(data[keys_head], *keys_tail)
+            elif strict:
+                raise KeyError(f'Invalid index')
+
     else:
         raise ValueError('Cannot delete primitive value')
 
 
-__all__ = ['NEW_LIST_ITEM', 'delete_item_tree', 'get_item_tree', 'set_item_tree_value']
+__all__ = ['NewListItemSingleton', 'NEW_LIST_ITEM', 'delete_json_tree_item', 'get_json_tree_item', 'set_json_tree_item']
+
+
+def tokenize_json_path(s: str):
+    return_tokens: list[Union[NewListItemSingleton, int, str]] = []
+    if not s:
+        return return_tokens
+    parts = s.split(JSON_OBJECT_SEP)
+    for part in parts:
+        if part == NEW_LIST_SYMBOL:
+            return_tokens.append(NEW_LIST_ITEM)
+        elif re_interger.match(part):
+            return_tokens.append(int(part))
+        else:
+            return_tokens.append(part)
+    return return_tokens
+
+
+def get_json_item(data, json_path='', default=None, strict=False):
+    return get_json_tree_item(data, *tokenize_json_path(json_path), default=default, strict=strict)
+
+
+def set_json_item(data, json_path='', value=None):
+    set_json_tree_item(data, *tokenize_json_path(json_path), value=value)
+
+
+def delete_json_item(data, json_path='', strict=False):
+    delete_json_tree_item(data, *tokenize_json_path(json_path), strict=strict)
--- lib/test/test_compound.py
+++ lib/test/test_compound.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lib.compounds import get_item_tree
+from lib.compounds import get_json_tree_item
 
 
 class JSONLocator(unittest.TestCase):
@@ -37,18 +37,20 @@ class JSONLocator(unittest.TestCase):
             ]
         }
         # 딕셔너리
-        self.assertEqual(get_item_tree(test_dict), {'aaa': {'bbb': {'ccc': 1, 'ddd': 0.5, 'eee': True, 'fff': False},
-                                                            'ggg': [10, 20, 'test', 'hello', 'world']},
-                                                    'hhh': [0.1, 0.2, [0.3, 0.4, 0.5], {'iii': 'python'}]})
-        self.assertEqual(get_item_tree(test_dict, 0), None)
-        self.assertEqual(get_item_tree(test_dict, 'aaa'), {'bbb': {'ccc': 1, 'ddd': 0.5, 'eee': True, 'fff': False},
-                                                           'ggg': [10, 20, 'test', 'hello', 'world']})
-        self.assertEqual(get_item_tree(test_dict, 'aaa', 'bbb'), {'ccc': 1, 'ddd': 0.5, 'eee': True, 'fff': False})
-        self.assertEqual(get_item_tree(test_dict, 'aaa', 'ggg'), [10, 20, 'test', 'hello', 'world'])
-        self.assertEqual(get_item_tree(test_dict, 'aaa', 'ggg', 2), 'test')
-        self.assertEqual(get_item_tree(test_dict, 'aaa', 10), None)
-        self.assertEqual(get_item_tree(test_dict, 'hhh'), [0.1, 0.2, [0.3, 0.4, 0.5], {'iii': 'python'}])
-        self.assertEqual(get_item_tree(test_dict, 'hhh', 3), {'iii': 'python'})
+        self.assertEqual(get_json_tree_item(test_dict),
+                         {'aaa': {'bbb': {'ccc': 1, 'ddd': 0.5, 'eee': True, 'fff': False},
+                                  'ggg': [10, 20, 'test', 'hello', 'world']},
+                          'hhh': [0.1, 0.2, [0.3, 0.4, 0.5], {'iii': 'python'}]})
+        self.assertEqual(get_json_tree_item(test_dict, 0), None)
+        self.assertEqual(get_json_tree_item(test_dict, 'aaa'),
+                         {'bbb': {'ccc': 1, 'ddd': 0.5, 'eee': True, 'fff': False},
+                          'ggg': [10, 20, 'test', 'hello', 'world']})
+        self.assertEqual(get_json_tree_item(test_dict, 'aaa', 'bbb'), {'ccc': 1, 'ddd': 0.5, 'eee': True, 'fff': False})
+        self.assertEqual(get_json_tree_item(test_dict, 'aaa', 'ggg'), [10, 20, 'test', 'hello', 'world'])
+        self.assertEqual(get_json_tree_item(test_dict, 'aaa', 'ggg', 2), 'test')
+        self.assertEqual(get_json_tree_item(test_dict, 'aaa', 10), None)
+        self.assertEqual(get_json_tree_item(test_dict, 'hhh'), [0.1, 0.2, [0.3, 0.4, 0.5], {'iii': 'python'}])
+        self.assertEqual(get_json_tree_item(test_dict, 'hhh', 3), {'iii': 'python'})
 
 
 if __name__ == '__main__':
