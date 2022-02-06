7b714666a76676a9d7c04bee6c6d6a87be877e00
strobi-bunni <dbrlgus8520@gmail.com>
2022-02-07 00:53:32 +0900

    refine project folder structure

---
+++ lib/compounds/__init__.py
@@ -0,0 +1,5 @@
+from .json_paths import NEW_LIST_ITEM, NewListItemSingleton, delete_json_item, delete_json_tree_item, get_json_item, \
+    get_json_tree_item, set_json_item, set_json_tree_item
+
+__all__ = ['NEW_LIST_ITEM', 'NewListItemSingleton', 'delete_json_item', 'delete_json_tree_item', 'get_json_item',
+           'get_json_tree_item', 'set_json_item', 'set_json_tree_item']
---
+++ lib/utils/__init__.py
@@ -0,0 +1,29 @@
+from typing import Any, Callable, Optional, TypeVar
+
+T = TypeVar('T')
+
+
+def coalesce(*items: T, pred: Callable[[T], Any] = bool) -> Optional[T]:
+    """items의 값 중 처음으로 pred의 조건을 만족하는 항목을 반환한다.
+
+    Parameters
+    ----------
+    items : T
+        항목들
+    pred : Callable, (T) -> Any
+        각각의 항목들에 대해서 조건을 판별할 함수
+
+    Returns
+    -------
+    first_true : T
+        처음으로 True인 값. 만약 없으면 None을 반환한다.
+    """
+    returns = None
+    for item in items:
+        if pred(item):
+            returns = item
+            break
+    return returns
+
+
+__all__ = ['coalesce']
