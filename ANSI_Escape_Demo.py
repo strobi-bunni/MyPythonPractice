b19960759f3765f643209d32c083cc679e48b846
strobi-bunni <dbrlgus8520@gmail.com>
2022-03-19 14:06:49 +0900

    code refined

--- ANSI_Escape_Demo.py
+++ ANSI_Escape_Demo.py
@@ -30,7 +30,7 @@ from textwrap import dedent
 
 code_mapping = [
     (0, 'Reset All'), (1, 'Bold'), (2, 'Faint'), (3, 'Italic'), (4, 'Underline'), (5, 'Slow Blink'), (6, 'Rapid Blink'),
-    (7, 'Invert'), (8, 'Hide'), (9, 'Crossed Out'), (21, 'Double Underline/Bold Off'), (22, 'Neither bold nor faint'),
+    (7, 'Invert'), (8, 'Hide'), (9, 'Crossed Out'), (21, 'Double Underline/Bold Off'), (22, 'Bold/Faint Off'),
     (23, 'Italic Off'), (24, 'Underline Off'), (25, 'Blink Off'), (27, 'Invert Off'), (28, 'Hide Off'),
     (29, 'Crossed Out Off'), (30, 'Foreground Black'), (31, 'Foreground Red'), (32, 'Foreground Green'),
     (33, 'Foreground Yellow'), (34, 'Foreground Blue'), (35, 'Foreground Magenta'), (36, 'Foreground Cyan'),
@@ -52,5 +52,5 @@ if __name__ == '__main__':
            
           Code    Description                   Example
           --------------------------------------------------------------------"""))
-for i, s in code_mapping:
-    print(f'{i:<8}{s:<30}\033[{i}m{s}\033[0m')
+    for i, s in code_mapping:
+        print(f'{i:<8}{s:<30}\033[{i}m{s}\033[0m')
