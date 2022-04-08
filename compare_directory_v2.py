76211c9a2a40d24ba0e00a7a29a756464580de3e
strobi-bunni <dbrlgus8520@gmail.com>
2022-04-08 18:35:35 +0900

    refactoring my old masterpiece #1

--- compare_directory_v2.py
+++ compare_directory_v2.py
@@ -10,7 +10,7 @@
 '<' - DEST_DIR에 있는 항목이 SOURCE_DIR에 있는 해당 항목보다 최신
 '>' - DEST_DIR에 있는 항목이 SOURCE_DIR에 있는 해당 항목보다 오래됨
 '!' - SOURCE_DIR와 DEST_DIR에 있는 항목이 서로 다르지만 날짜가 같음
-'@' - 비교 결과 미정
+'@' - 비교 결과 미정(일반적으로는 볼 수 없음)
 
 항목의 종류는 다음과 같은 기호로 표시됩니다.
 
@@ -30,7 +30,26 @@ import sys
 from collections import Counter
 from filecmp import dircmp
 from pathlib import Path, PurePath
-from typing import Callable, Iterator, List, Literal, NamedTuple, Tuple, Union
+from typing import Iterator, List, Literal, NamedTuple, Tuple, Union
+
+# 비교 결과를 나타내기 위한 기호
+RESULT_SAME = '='  # 같은 대상
+RESULT_CREATED = '+'  # 생성됨
+RESULT_DELETED = '-'  # 삭제됨
+RESULT_DIFF = '!'  # 서로 다름(src != dst)
+RESULT_NEWER = '<'  # dst의 파일이 최신임(src.mtime < dst_mtime)
+RESULT_OLDER = '>'  # src의 파일이 최신임(src.mtime > dst_mtime)
+RESULT_UNDEFINED = '@'  # 알 수 없음
+
+# 경로의 타입을 나타내기 위한 기호
+PATHTYPE_DIRECTORY = 'D'
+PATHTYPE_FILE = 'F'
+PATHTYPE_MOUNT = 'M'
+PATHTYPE_SYMLINK = 'L'
+PATHTYPE_BLOCKDEV = 'B'
+PATHTYPE_CHARDEV = 'C'
+PATHTYPE_FIFO = 'I'
+PATHTYPE_SOCKET = 'S'
 
 T_ItemTypes = Literal['directory', 'file', 'mount', 'symlink', 'blockdev', 'chardev', 'fifo', 'socket']
 T_CompareResultTypes = Literal['same', 'created', 'deleted', 'diff', 'newer', 'older', 'undefined']
@@ -38,8 +57,8 @@ item_type_abbrev = {'directory': 'D', 'file': 'F', 'mount': 'M', 'symlink': 'L',
                     'blockdev': 'B', 'chardev': 'C', 'fifo': 'I', 'socket': 'S'}
 compare_result_abbrev = {'same': '=', 'created': '+', 'deleted': '-', 'diff': '!',
                          'newer': '<', 'older': '>', 'undefined': '@'}
-item_type_sort_order = {'directory': 1, 'file': 2, 'mount': 2, 'symlink': 2,
-                        'blockdev': 2, 'chardev': 2, 'fifo': 2, 'socket': 2}
+item_type_sort_order = {PATHTYPE_DIRECTORY: 1, PATHTYPE_FILE: 2, PATHTYPE_MOUNT: 2, PATHTYPE_SYMLINK: 2,
+                        PATHTYPE_BLOCKDEV: 2, PATHTYPE_CHARDEV: 2, PATHTYPE_FIFO: 2, PATHTYPE_SOCKET: 2}
 item_type_group_id = {'directory': 1, 'file': 0, 'mount': 3, 'symlink': 2,
                       'blockdev': 3, 'chardev': 3, 'fifo': 3, 'socket': 3}
 
@@ -49,13 +68,17 @@ compare_result_color_code = {'same': '\033[39m', 'created': '\033[92m', 'deleted
 
 
 class CompareResult(NamedTuple):
-    path: PurePath
-    item_type: T_ItemTypes
-    compare_result: T_CompareResultTypes
+    path: PurePath  # src, dst에 대한 상대 경로
+    item_type: str  # PATHTYPE_*
+    compare_result: str  # RESULT_*
 
     def __str__(self):
-        return f'{compare_result_abbrev[self.compare_result]}{item_type_abbrev[self.item_type]}  ' \
+        return f'{self.compare_result}{self.item_type}  ' \
                f'{path_to_string(self.path, override_is_dir=(self.item_type == "directory"))}'
+
+    # 기본 정렬 순서
+    def __lt__(self, other):
+        return path_to_string(self.path) < path_to_string(other.path)
 
 
 def compare_dir(src: Path, dst: Path, working_dir: PurePath = PurePath('.')) -> Iterator[CompareResult]:
@@ -79,7 +102,7 @@ def compare_dir(src: Path, dst: Path, working_dir: PurePath = PurePath('.')) -> 
     # 이름과 내용이 같은 파일들을 처리한다.
     for name in dcmp.same_files:
         src_item = src / name
-        yield CompareResult(working_dir / name, get_type(src_item), 'same')
+        yield CompareResult(working_dir / name, get_type(src_item), RESULT_SAME)
 
     # 이름은 같지만 내용이 다른 파일들을 처리한다.
     for name in dcmp.diff_files:
@@ -87,20 +110,20 @@ def compare_dir(src: Path, dst: Path, working_dir: PurePath = PurePath('.')) -> 
         dst_item = dst / name
 
         # 파일의 수정 시각을 비교해서 newer 혹은 older를 판단한다.
-        compare_result_type = 'newer' if dst_item.stat().st_mtime > src_item.stat().st_mtime \
-            else 'older' if dst_item.stat().st_mtime < src_item.stat().st_mtime \
-            else 'diff'
+        compare_result_type = RESULT_NEWER if src_item.stat().st_mtime < dst_item.stat().st_mtime \
+            else RESULT_OLDER if src_item.stat().st_mtime > dst_item.stat().st_mtime \
+            else RESULT_DIFF
         yield CompareResult(working_dir / name, get_type(src_item), compare_result_type)  # noqa
 
     # 동일한 폴더를 찾는다.(폴더의 이름이 동일한지의 여부, 안의 내용은 상관없다.)
     for name in dcmp.common_dirs:
-        yield CompareResult(working_dir / name, 'directory', 'same')
+        yield CompareResult(working_dir / name, PATHTYPE_DIRECTORY, RESULT_SAME)
         # 안의 내용물을 재귀적으로 계산한다.
         yield from compare_dir(src / name, dst / name, working_dir / name)
 
     # src에만 있는 항목들을 찾아서 deleted로 저장한다.
     for name in dcmp.left_only:
-        yield CompareResult(working_dir / name, get_type(src / name), 'deleted')
+        yield CompareResult(working_dir / name, get_type(src / name), RESULT_DELETED)
 
         # 만약에 폴더라면 안의 내용들을 전부 deleted로 간주한다.
         if (src / name).is_dir():
@@ -108,7 +131,7 @@ def compare_dir(src: Path, dst: Path, working_dir: PurePath = PurePath('.')) -> 
 
     # dst에만 있는 항목들을 찾아서 created로 저장한다.
     for name in dcmp.right_only:
-        yield CompareResult(working_dir / name, get_type(dst / name), 'created')
+        yield CompareResult(working_dir / name, get_type(dst / name), RESULT_CREATED)
         # 만약에 폴더라면 안의 내용들을 전부 created로 간주한다.
         if (dst / name).is_dir():
             yield from mark_as_created_recursive((dst / name), working_dir / name)
@@ -119,28 +142,28 @@ def compare_dir(src: Path, dst: Path, working_dir: PurePath = PurePath('.')) -> 
         dst_type = get_type(dst / name)
         # 만약에 타입이 다르다면 src의 항목들을 deleted로, dst의 항목들을 created로 간주한다.
         if src_type != dst_type:
-            yield CompareResult(working_dir / name, src_type, 'deleted')
-            yield CompareResult(working_dir / name, dst_type, 'created')
-            if src_type == 'directory':
+            yield CompareResult(working_dir / name, src_type, RESULT_DELETED)
+            yield CompareResult(working_dir / name, dst_type, RESULT_CREATED)
+            if src_type == PATHTYPE_DIRECTORY:
                 yield from mark_as_deleted_recursive((src / name), working_dir / name)
-            if dst_type == 'directory':
+            if dst_type == PATHTYPE_DIRECTORY:
                 yield from mark_as_created_recursive((dst / name), working_dir / name)
         # 만약 타입이 같다면 비교할 수 없다고 간주한다.
         else:
-            yield CompareResult(working_dir / name, src_type, 'undefined')
+            yield CompareResult(working_dir / name, src_type, RESULT_UNDEFINED)
 
 
 def mark_as_deleted_recursive(path: Path, working_dir=PurePath('.')) -> Iterator[CompareResult]:
     for item in path.glob('**/*'):
-        yield CompareResult(working_dir / (item.relative_to(path)), get_type(item), 'deleted')
+        yield CompareResult(working_dir / (item.relative_to(path)), get_type(item), RESULT_DELETED)
 
 
 def mark_as_created_recursive(path: Path, working_dir=PurePath('.')) -> Iterator[CompareResult]:
     for item in path.glob('**/*'):
-        yield CompareResult(working_dir / (item.relative_to(path)), get_type(item), 'created')
+        yield CompareResult(working_dir / (item.relative_to(path)), get_type(item), RESULT_CREATED)
 
 
-def get_type(p: Path) -> T_ItemTypes:
+def get_type(p: Path) -> str:
     """경로 p의 유형을 구한다.
 
     Parameters
@@ -150,34 +173,25 @@ def get_type(p: Path) -> T_ItemTypes:
 
     Returns
     -------
-    type_ : {'directory', 'file', 'mount', 'symlink', 'blockdev', 'chardev', 'fifo', 'socket'}
+    type_ : str
         경로의 유형
     """
     if p.is_file():
-        return 'file'  # noqa
+        return PATHTYPE_FILE
     elif p.is_dir():
-        return 'directory'  # noqa
+        return PATHTYPE_DIRECTORY
     elif p.is_mount():
-        return 'mount'  # noqa
+        return PATHTYPE_MOUNT
     elif p.is_symlink():
-        return 'symlink'  # noqa
+        return PATHTYPE_SYMLINK
     elif p.is_block_device():
-        return 'blockdev'  # noqa
+        return PATHTYPE_BLOCKDEV
     elif p.is_char_device():
-        return 'chardev'  # noqa
+        return PATHTYPE_CHARDEV
     elif p.is_fifo():
-        return 'fifo'  # noqa
+        return PATHTYPE_FIFO
     else:
-        return 'socket'  # noqa
-
-
-def get_result_abbrev(x: CompareResult) -> str:
-    """
-
-    :param x:
-    :return:
-    """
-    return compare_result_abbrev[x.compare_result] + item_type_abbrev[x.item_type]
+        return PATHTYPE_SOCKET
 
 
 def expand_parents_of_result(x: CompareResult) -> List[CompareResult]:
@@ -193,7 +207,8 @@ def expand_parents_of_result(x: CompareResult) -> List[CompareResult]:
     """
     return_list: List[CompareResult] = []
     for parent_path in reversed(x.path.parents):
-        return_list.append(CompareResult(path=parent_path, item_type='directory', compare_result='undefined'))
+        return_list.append(
+            CompareResult(path=parent_path, item_type=PATHTYPE_DIRECTORY, compare_result=RESULT_UNDEFINED))
     return_list.append(x)
     return return_list
 
@@ -225,13 +240,6 @@ def path_to_string(path: Union[Path, PurePath], override_is_dir=False) -> str:
     return p
 
 
-def print_result(x: CompareResult, colored=False, file=sys.stdout):
-    color_code_prefix = compare_result_color_code[x.compare_result] if colored else ''
-    color_code_suffix = '\033[0m' if colored else ''
-    name = path_to_string(x.path, override_is_dir=(x.item_type == 'directory'))
-    print(f'{color_code_prefix}{get_result_abbrev(x):<4}{name}{color_code_suffix}', file=file)
-
-
 def print_summary(res: List[CompareResult], colored=False, file=sys.stdout):
     cnt = Counter((r.compare_result, item_type_group_id[r.item_type]) for r in res)
     for compare_result in ['same', 'created', 'deleted', 'newer', 'older', 'diff', 'undefined']:
@@ -243,30 +251,13 @@ def print_summary(res: List[CompareResult], colored=False, file=sys.stdout):
               file=file)
 
 
-def print_header(source_path: Path, dest_path: Path, colored=False, file=sys.stdout):
-    title_color_code_prefix = '\033[1m' if colored else ''
-    title_color_code_suffix = '\033[0m' if colored else ''
-    print(f'{title_color_code_prefix}Source: {path_to_string(source_path)}{title_color_code_suffix}', file=file)
-    print(f'{title_color_code_prefix}Dest:   {path_to_string(dest_path)}{title_color_code_suffix}', file=file)
-
-
-def get_filter_pred(filter_syntax: str) -> Callable[[CompareResult], bool]:
-    compare_result_types_to_output = []
-    for (k, v) in compare_result_abbrev.items():
-        if v in filter_syntax:
-            compare_result_types_to_output.append(k)
-    return lambda x: x.compare_result in compare_result_types_to_output
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('source', metavar='SOURCE_DIR', type=str, help='원본 폴더')
     parser.add_argument('dest', metavar='DEST_DIR', type=str, help='대상 폴더')
-    parser.add_argument('-c', '--color', action='store_true', dest='color', help='색상으로 표시합니다.')
-    parser.add_argument('-i', '--include-same-files', action='store_true', dest='include_same_files',
-                        help='같은 파일들을 출력합니다.')
+    # parser.add_argument('-c', '--color', action='store_true', dest='color', help='색상으로 표시합니다.')
     parser.add_argument('-s', '--summary', action='store_true', dest='summary', help='요약을 출력합니다.')
-    parser.add_argument('-f', '--filter', dest='filter_syntax', type=str, default='=+-!<>@',
+    parser.add_argument('-f', '--filter', dest='filter_syntax', type=str, default='+-!<>@',
                         help='대상 결과만을 보여줍니다.\n= + - ! < > @ 중 하나 이상을 조합합니다.')
     parser.add_argument('-o', '--output', dest='output_file', type=argparse.FileType('w', encoding='utf-8'),
                         default=sys.stdout, help='출력할 파일')
@@ -274,18 +265,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     src_dir, dst_dir = Path(args.source), Path(args.dest)
-    print_header(src_dir, dst_dir, colored=args.color, file=args.output_file)
+    print(f'#Source: {path_to_string(src_dir)}', file=args.output_file)
+    print(f'#Dest:   {path_to_string(dst_dir)}', file=args.output_file)
     print(file=args.output_file)
 
     result = sorted(compare_dir(src_dir, dst_dir), key=sort_key_for_directory_first)
-
-    filter_pred = get_filter_pred(args.filter_syntax)
     for i in result:
-        if i.compare_result != 'same' or args.include_same_files:
-            # Short circuit logic: i.compare_result!=same이거나 i.compare_result==same and args.include_same_files일때
-            if filter_pred(i):
-                print_result(i, colored=args.color, file=args.output_file)
+        if i.compare_result in args.filter_syntax:
+            print(i, file=args.output_file)
 
     if args.summary:
         print(file=args.output_file)
-        print_summary(result, colored=args.color, file=args.output_file)
+        print_summary(result, colored=False, file=args.output_file)
