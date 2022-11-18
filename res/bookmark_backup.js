0d1625effd4fdbb196ebd22fc9329e4fbafb8846
strobi-bunni <dbrlgus8520@gmail.com>
2022-11-18 21:12:10 +0900

    Refactor: Remove redundant inline style toggling

--- res/bookmark_backup.js
+++ res/bookmark_backup.js
@@ -1,13 +1,7 @@
-var collapsibles = document.getElementsByClassName('collapsible');
+let collapsibles = document.getElementsByClassName('collapsible');
 
 for (let collapsible of collapsibles) {
     collapsible.addEventListener("click", function() {
         this.classList.toggle("active");
-        let content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
     });
 }
