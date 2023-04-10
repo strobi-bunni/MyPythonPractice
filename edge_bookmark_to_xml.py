4601cad888cf677be230ec8c1c4c12cf75424a6d
strobi-bunni <dbrlgus8520@gmail.com>
2023-04-10 22:56:48 +0900

    Convert timestamps to readable format

--- edge_bookmark_to_xml.py
+++ edge_bookmark_to_xml.py
@@ -4,22 +4,33 @@
 """
 import json
 import os
+from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from xml.dom.minidom import parseString
 from xml.etree.ElementTree import Element, SubElement, tostring
+
+TIMESTAMP_EPOCH = datetime(1601, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+
+
+def convert_timestamp(t: str) -> str:
+    # 타임스탬프: 1601-01-01T00:00:00Z로부터 경과한 마이크로초 단위의 시간
+    ts = int(t)
+    return (TIMESTAMP_EPOCH + timedelta(seconds=(ts // 1000000), microseconds=(ts % 1000000))) \
+        .strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
 def insert_data_in_elem(data: dict, root_elem: Element):
     if data['type'] == 'folder':
         folder_elem = SubElement(root_elem, 'BookmarkFolder',
-                                 attrib={'name': data['name'], 'id': data['id'], 'date_added': data['date_added'],
-                                         'date_modified': data['date_modified']})
+                                 attrib={'name': data['name'], 'id': data['id'],
+                                         'date_added': convert_timestamp(data['date_added']),
+                                         'date_modified': convert_timestamp(data['date_modified'])})
 
         for child in data['children']:
             insert_data_in_elem(child, folder_elem)
     else:
         SubElement(root_elem, 'Bookmark',
-                   attrib={'id': data['id'], 'date_added': data['date_added'], 'href': data['url']},
+                   attrib={'id': data['id'], 'date_added': convert_timestamp(data['date_added']), 'href': data['url']},
                    ).text = data['name']
 
 
